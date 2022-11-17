e375ea8465a5e113330547e87003af6c4dbad141
macieowens <112588931+macieowens@users.noreply.github.com>
2022-11-16 20:18:03 -0600

    initial commit'

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+namespace selection_statements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int favoriteNumber = 11;
+
+            //Console.WriteLine("Guess my favorite number, hint: 1 and 1 together");
+            //int userGuess = int.Parse(Console.ReadLine());
+
+            //if (userGuess < favoriteNumber)
+            //{
+            //Console.WriteLine("Too Low! Try again");
+            //}
+            //else if (userGuess > favoriteNumber)
+            //{
+            //Console.WriteLine("Too high! Try again");       
+            //}
+            //else if (userGuess == favoriteNumber)
+            //{
+            //Console.WriteLine("Good Job!");
+            //}
+            //else
+            //{
+            //Console.WriteLine("Stop trying");
+            //}
+
+            string subject = "C";
+
+            Console.WriteLine("What's your favorite subject?");
+            string userSubject = Console.ReadLine();
+
+            switch (userSubject)
+            {
+                case "C#":
+                    Console.WriteLine("Congradulations! You are in the right class");
+                    break;
+                case "Java":
+                    Console.WriteLine("C# is better than Java.");
+                    break;
+                default:
+                    Console.WriteLine("We teach and learn code.");
+                    break;
+            }
+
+        }
+    }
+}
